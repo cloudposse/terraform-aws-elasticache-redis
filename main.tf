7bd355c6e6a5cc5f007229ea11ebadbe7164474c
Erik Osterman <erik@cloudposse.com>
2017-11-02 13:29:58 -0700

    Fix Disabled Outputs (#9)

--- main.tf
+++ main.tf
@@ -111,12 +111,12 @@ resource "aws_cloudwatch_metric_alarm" "cache_memory" {
 }
 
 module "dns" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.0"
+  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.1"
   enabled   = "${var.enabled}"
   namespace = "${var.namespace}"
   name      = "${var.name}"
   stage     = "${var.stage}"
   ttl       = 60
   zone_id   = "${var.zone_id}"
-  records   = ["${aws_elasticache_replication_group.default.primary_endpoint_address}"]
+  records   = ["${aws_elasticache_replication_group.default.*.primary_endpoint_address}"]
 }
