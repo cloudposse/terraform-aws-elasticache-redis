767121d1163ddc7789567b2f4b3a1371520e0b16
Erik Osterman <erik@cloudposse.com>
2017-11-02 13:22:59 -0700

    pin to 0.2.1

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_cloudwatch_metric_alarm" "cache_memory" {
 }
 
 module "dns" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=fix-disabled-output"
+  source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.1"
   enabled   = "${var.enabled}"
   namespace = "${var.namespace}"
   name      = "${var.name}"
