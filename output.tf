2055293796ef7c8bd9bdc9df4524c4a4ccf5febf
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-04 15:28:19 +0300

    Migrate to README.yaml format, add travis

--- output.tf
+++ output.tf
@@ -1,15 +1,19 @@
 output "id" {
-  value = "${join("", aws_elasticache_replication_group.default.*.id)}"
+  value       = "${join("", aws_elasticache_replication_group.default.*.id)}"
+  description = "Redis cluster id"
 }
 
 output "security_group_id" {
-  value = "${join("", aws_security_group.default.*.id)}"
+  value       = "${join("", aws_security_group.default.*.id)}"
+  description = "Security group id"
 }
 
 output "port" {
-  value = "${var.port}"
+  value       = "${var.port}"
+  description = "Redis port"
 }
 
 output "host" {
-  value = "${module.dns.hostname}"
+  value       = "${module.dns.hostname}"
+  description = "Redis host"
 }
