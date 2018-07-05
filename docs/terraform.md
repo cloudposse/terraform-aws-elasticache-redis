d7b57edc447dc2b9463920875f22bb90da5dd91e
Vadim Hleif <vadzim.hleif@gmail.com>
2018-07-05 15:46:32 +0300

    Migrate to README.yaml format, add travis (#13)

---
+++ docs/terraform.md
@@ -0,0 +1,39 @@
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| alarm_actions | Alarm action list | list | `<list>` | no |
+| alarm_cpu_threshold_percent | CPU threshold alarm level | string | `75` | no |
+| alarm_memory_threshold_bytes | Ram threshold alarm level | string | `10000000` | no |
+| apply_immediately | Apply changes immediately | string | `true` | no |
+| attributes | Additional attributes (_e.g._ "1") | list | `<list>` | no |
+| automatic_failover | Automatic failover (Not available for T1/T2 instances) | string | `false` | no |
+| availability_zones | Availability zone ids | list | `Availability zone ids` | no |
+| cluster_size | Count of nodes in cluster | string | `1` | no |
+| delimiter | Delimiter between `name`, `namespace`, `stage` and `attributes` | string | `-` | no |
+| enabled | Set to false to prevent the module from creating any resources | string | `true` | no |
+| engine_version | Redis engine version | string | `4.0.10` | no |
+| family | Redis family | string | `redis4.0` | no |
+| instance_type | Elastic cache instance type | string | `cache.t2.micro` | no |
+| maintenance_window | Maintenance window | string | `wed:03:00-wed:04:00` | no |
+| name | Name | string | `redis` | no |
+| namespace | Namespace | string | `global` | no |
+| notification_topic_arn | Notification topic arn | string | `10000000` | no |
+| port | Redis port | string | `6379` | no |
+| security_groups | AWS security group ids | list | `<list>` | no |
+| stage | Stage | string | `default` | no |
+| subnets | AWS subnet ids | list | `<list>` | no |
+| tags | Additional tags (_e.g._ map("BusinessUnit","ABC") | map | `<map>` | no |
+| vpc_id | AWS VPC id | string | `REQUIRED` | no |
+| zone_id | Route53 DNS Zone id | string | `false` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| host | Redis host |
+| id | Redis cluster id |
+| port | Redis port |
+| security_group_id | Security group id |
+
--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "namespace" {
-  default = "global"
+  default     = "global"
+  description = "Namespace"
 }
 
 variable "enabled" {
@@ -8,93 +9,119 @@ variable "enabled" {
 }
 
 variable "stage" {
-  default = "default"
+  default     = "default"
+  description = "Stage"
 }
 
 variable "name" {
-  default = "redis"
+  default     = "redis"
+  description = "Name"
 }
 
 variable "security_groups" {
-  type = "list"
+  type        = "list"
+  default     = []
+  description = "AWS security group ids"
 }
 
 variable "vpc_id" {
-  default = ""
+  default     = "REQUIRED"
+  description = "AWS VPC id"
 }
 
 variable "subnets" {
-  type    = "list"
-  default = []
+  type        = "list"
+  description = "AWS subnet ids"
+  default     = []
 }
 
 variable "maintenance_window" {
-  default = "wed:03:00-wed:04:00"
+  default     = "wed:03:00-wed:04:00"
+  description = "Maintenance window"
 }
 
 variable "cluster_size" {
-  default = "1"
+  default     = "1"
+  description = "Count of nodes in cluster"
 }
 
 variable "port" {
-  default = "6379"
+  default     = "6379"
+  description = "Redis port"
 }
 
 variable "instance_type" {
-  default = "cache.t2.micro"
+  default     = "cache.t2.micro"
+  description = "Elastic cache instance type"
 }
 
 variable "family" {
-  default = "redis4.0"
+  default     = "redis4.0"
+  description = "Redis family "
 }
 
 variable "engine_version" {
-  default = "4.0.10"
+  default     = "4.0.10"
+  description = "Redis engine version"
 }
 
 variable "notification_topic_arn" {
-  default = ""
+  default     = "10000000"
+  description = "Notification topic arn"
 }
 
 variable "alarm_cpu_threshold_percent" {
-  default = "75"
+  default     = "75"
+  description = "CPU threshold alarm level"
 }
 
 variable "alarm_memory_threshold_bytes" {
   # 10MB
-  default = "10000000"
+  default     = "10000000"
+  description = "Ram threshold alarm level"
 }
 
 variable "alarm_actions" {
-  type    = "list"
-  default = []
+  type        = "list"
+  description = "Alarm action list"
+  default     = []
 }
 
 variable "apply_immediately" {
-  default = "true"
+  default     = "true"
+  description = "Apply changes immediately"
 }
 
 variable "automatic_failover" {
-  default = "false"
+  default     = "false"
+  description = "Automatic failover (Not available for T1/T2 instances)"
 }
 
 variable "availability_zones" {
-  type = "list"
+  type        = "list"
+  description = "Availability zone ids"
+  default     = "Availability zone ids"
 }
 
-variable "zone_id" {}
+variable "zone_id" {
+  default     = "false"
+  description = "Route53 DNS Zone id"
+}
 
 variable "delimiter" {
-  type    = "string"
-  default = "-"
+  type        = "string"
+  default     = "-"
+  description = "Delimiter between `name`, `namespace`, `stage` and `attributes`"
 }
 
 variable "attributes" {
-  type    = "list"
-  default = []
+  type        = "list"
+  description = "Additional attributes (_e.g._ \"1\")"
+  default     = []
 }
 
 variable "tags" {
-  type    = "map"
-  default = {}
+  type        = "map"
+  description = "Additional tags (_e.g._ map(\"BusinessUnit\",\"ABC\")"
+  default     = {}
 }
