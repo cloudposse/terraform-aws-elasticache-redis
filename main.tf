04421e50b8d80ad0aff3bcc4fad579abe7b45e24
Štěpán Vraný <vrany.stepan@gmail.com>
2019-09-16 11:52:20 +0200

    fix ref.

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_elasticache_parameter_group" "default" {
 resource "aws_elasticache_replication_group" "default" {
   count = var.enabled ? 1 : 0
 
-  auth_token                    = transit_encryption_enabled ? var.auth_token : null
+  auth_token                    = var.transit_encryption_enabled ? var.auth_token : null
   replication_group_id          = var.replication_group_id == "" ? module.label.id : var.replication_group_id
   replication_group_description = module.label.id
   node_type                     = var.instance_type
