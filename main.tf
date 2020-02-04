9f0ee51827234627052951321a814270160ce763
Jim Park <jimp@scribd.com>
2020-02-04 15:39:51 -0800

    feat: add ClusterEnabled: true default parameter (unless overwritten)

--- main.tf
+++ main.tf
@@ -67,8 +67,10 @@ resource "aws_elasticache_parameter_group" "default" {
   name   = module.label.id
   family = var.family
 
+
+
   dynamic "parameter" {
-    for_each = var.parameter
+    for_each = var.cluster_mode_enabled ? concat([{ "name" = "cluster-enabled" , "value" = "yes" }], var.parameter) : var.parameter
     content {
       name  = parameter.value.name
       value = parameter.value.value
