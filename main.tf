147603ad533bbec50369c01ccbe49b8052414716
Ville Gustafsson <gusse88@gmail.com>
2021-02-10 14:40:57 +0200

    allow empty list of availability_zones
    availability_zones parameter shouldn't be required

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "aws_elasticache_replication_group" "default" {
   number_cache_clusters         = var.cluster_mode_enabled ? null : var.cluster_size
   port                          = var.port
   parameter_group_name          = join("", aws_elasticache_parameter_group.default.*.name)
-  availability_zones            = var.cluster_mode_enabled ? null : slice(var.availability_zones, 0, var.cluster_size)
+  availability_zones            = length(var.availability_zones) == 0 ? null : slice(var.availability_zones, 0, var.cluster_size)
   automatic_failover_enabled    = var.automatic_failover_enabled
   subnet_group_name             = local.elasticache_subnet_group_name
   security_group_ids            = var.use_existing_security_groups ? var.existing_security_groups : [join("", aws_security_group.default.*.id)]
