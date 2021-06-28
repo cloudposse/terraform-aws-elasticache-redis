dd90bd6dd1c1c262847c042c6802376c7230886e
Callum <callumccr@gmail.com>
2021-06-28 14:10:14 +0100

    added count meta-argument on the security_group module

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ locals {
 }
 
 module "security_group" {
-  source  = "cloudposse/security-group/aws"
-  version = "0.3.1"
-
+  source          = "cloudposse/security-group/aws"
+  version         = "0.3.1"
+  count           = var.create_security_group == true ? 1 : 0
   use_name_prefix = var.security_group_use_name_prefix
   rules           = var.security_group_rules
   description     = var.security_group_description
