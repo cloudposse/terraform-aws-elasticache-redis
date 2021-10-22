e4d2ee50c28894a918981744ee5052782c49d883
Brad Janke <bradj@users.noreply.github.com>
2021-10-22 15:02:37 -0500

    Chore: bumps security group module version to 0.4.1

--- main.tf
+++ main.tf
@@ -16,11 +16,9 @@ locals {
 
 module "security_group" {
   source  = "cloudposse/security-group/aws"
-  version = "0.3.1"
+  version = "0.4.1"
 
-  use_name_prefix = var.security_group_use_name_prefix
   rules           = var.security_group_rules
-  description     = var.security_group_description
   vpc_id          = var.vpc_id
 
   enabled = local.security_group_enabled
