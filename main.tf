3181a370412a2c4ff615af91b5884e6336675aa8
nitrocode <nitrocode@users.noreply.github.com>
2021-12-14 13:42:19 -0600

    Set auto failover to true if cluster mode enabled

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ resource "aws_elasticache_replication_group" "default" {
   port                          = var.port
   parameter_group_name          = join("", aws_elasticache_parameter_group.default.*.name)
   availability_zones            = length(var.availability_zones) == 0 ? null : [for n in range(0, var.cluster_size) : element(var.availability_zones, n)]
-  automatic_failover_enabled    = var.automatic_failover_enabled
+  automatic_failover_enabled    = var.cluster_mode_enabled ? true : var.automatic_failover_enabled
   multi_az_enabled              = var.multi_az_enabled
   subnet_group_name             = local.elasticache_subnet_group_name
   # It would be nice to remove null or duplicate security group IDs, if there are any, using `compact`,
