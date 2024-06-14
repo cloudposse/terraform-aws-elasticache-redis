7f276a0d496310f004b31023a5ec7e9838434023
Andres Montalban <andres.m@miro.com>
2024-06-14 08:09:56 -0300

    fix: Default transit_encryption_mode to null if var.transit_encryption_enabled is false

--- main.tf
+++ main.tf
@@ -178,7 +178,7 @@ resource "aws_elasticache_replication_group" "default" {
   engine_version             = var.engine_version
   at_rest_encryption_enabled = var.at_rest_encryption_enabled
   transit_encryption_enabled = var.transit_encryption_enabled
-  transit_encryption_mode    = var.transit_encryption_mode
+  transit_encryption_mode    = var.transit_encryption_enabled ? var.transit_encryption_mode : null
   kms_key_id                 = var.at_rest_encryption_enabled ? var.kms_key_id : null
   snapshot_name              = var.snapshot_name
   snapshot_arns              = var.snapshot_arns
